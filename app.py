0ac01223d05d5402490422eefad643604fe62c59
thivatharanj <j.thiva@gmail.com>
2023-04-01 23:59:33 -0500

    requirements.txt added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/query_sparc1', methods=['GET'])
+@app.route('/query_sparc', methods=['GET'])
 def get_query_records():
     input_string = request.args.get('input_string')
     try:
@@ -26,6 +26,7 @@ def post_query_records():
     try:
         query = get_sparc_query(input_string)
         result = get_sparc_result(query)
+        result = result.replace("_", " ")
         response = jsonify({'result': result})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
@@ -33,12 +34,26 @@ def post_query_records():
         return jsonify({'error': 'data not found', "reason": str(err)})
 
 
-def get_sparc_query(input_string="working hours"):
+def get_sparc_query(input_string):
     # Todo need to implement som AI work to map query and input string
-    return 'opening_hours(monday,X)'
+
+    if 'borrow' in input_string and 'book' in input_string:
+        book_type = 'General books'
+        return f"borrowing_period('{book_type}', B) :- book('book1', '{book_type}'). #maximize {{ B }}. #show B : borrowing_period('{book_type}', B), book('book1', '{book_type}')."
+    elif 'lost' in input_string and 'book' in input_string:
+        # return "lost_book_policy(P) :- #show P : lost_book_policy(P)."
+        return "lost_book_policy(P)"
+    elif 'opening' in input_string and 'hours' in input_string and 'weekdays' in input_string:
+        return "opening_hours(weekdays,X)"
+    elif 'opening' in input_string and 'hours' in input_string and 'weekends' in input_string:
+        return "opening_hours(weekends,X)"
+    elif 'opening' in input_string and 'hours' in input_string:
+        return "opening_hours(Y,X)"
+    else:
+        return None
 
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5001)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- sprc_connector.py
+++ sprc_connector.py
@@ -8,22 +8,22 @@ def get_sparc_result(query):
                "editor":
                    """ sorts
                    #people={tommy,alex,john,daniel,sarah,peter,lino}.
+                   #policy= {if_a_book_is_lost_the_borrower_must_pay_for_the_cost_of_the_book_plus_a_processing_fee}.
                    #book={book1,book2}.
                    #book_categories={drama, crime, fantasy, scientific}.
-                   #day={monday, tuesday, sunday}.
+                   #day={monday, tuesday, wednesday, thursday, friday, saturday, sunday, weekdays, weekends}.
                    #hours={morning_8_to_6pm, morning_8_to_12pm}.
                    predicates
                    categories(#book_categories,#book).
                    book_author(#people,#book).
                    opening_hours(#day,#hours).
+                   lost_book_policy(#policy).
                    rules
-                   opening_hours(monday, morning_8_to_6pm).
-                   opening_hours(sunday, morning_8_to_12pm).
+                   opening_hours(weekdays, morning_8_to_6pm).
+                   opening_hours(weekends, morning_8_to_12pm).
+                   lost_book_policy(If_a_book_is_lost_the_borrower_must_pay_for_the_cost_of_the_book_plus_a_processing_fee). 
                    """}
 
     x = requests.post(url, headers=headers, data=payload)
     print(x.text)
     return x.text
-
-
-# get_sparc_result('opening_hours(monday,X)')
