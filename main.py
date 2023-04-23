1d41f35fd31d7958d2b14bd9933c7f60a80e9f2b
thivatharanj <j.thiva@gmail.com>
2023-04-23 10:11:14 -0500

    updated UI and ASP

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# encoding: utf-8
 import json
 from flask import Flask, request, jsonify
 from sprc_connector import get_sparc_result
@@ -9,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/query_sparc1', methods=['GET'])
+@app.route('/query_sparc', methods=['GET'])
 def get_query_records():
     input_string = request.args.get('input_string')
     try:
@@ -26,6 +24,8 @@ def post_query_records():
     try:
         query = get_sparc_query(input_string)
         result = get_sparc_result(query)
+        result = result.replace(",_", "__")
+        result = result.replace("_", " ")
         response = jsonify({'result': result})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
@@ -33,12 +33,40 @@ def post_query_records():
         return jsonify({'error': 'data not found', "reason": str(err)})
 
 
-def get_sparc_query(input_string="working hours"):
-    # Todo need to implement som AI work to map query and input string
-    return 'opening_hours(monday,X)'
+def get_sparc_query(input_string):
+
+    categories = ['drama', 'crime', 'fantasy', 'scientific', 'fiction', 'story']
+    input_string = input_string.lower()
+
+    if 'lost' in input_string and 'book' in input_string:
+        return "lost_book_policy(P)"
+    elif 'opening' in input_string and 'hours' in input_string and 'weekdays' in input_string:
+        return "opening_hours(weekdays,X)"
+    elif 'opening' in input_string and 'hours' in input_string and 'weekends' in input_string:
+        return "opening_hours(weekends,X)"
+    elif 'opening' in input_string and 'hours' in input_string:
+        return "opening_hours(Y,X)"
+    elif 'ebook' in input_string and 'available' in input_string:
+        return "ebook_available(X)"
+    elif 'type' in input_string and 'information' in input_string:
+        return "information_type(X)"
+    elif 'how many' in input_string and 'check out' in input_string:
+        return "max_books(X)"
+    elif 'how long' in input_string and 'borrow' in input_string:
+        return "borrowing_period(X, Y)"
+    elif 'return' in input_string and 'check' in input_string:
+        return "return_policy(X)"
+    elif 'recommend' in input_string or any(word in input_string for word in categories):
+        for word in categories:
+            if word in input_string:
+                return "category({0},X)".format(word)
+        return "category(Y,X)"
+
+    else:
+        return None
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- sprc_connector.py
+++ sprc_connector.py
@@ -8,20 +8,50 @@ def get_sparc_result(query):
                "editor":
                    """ sorts
                    #people={tommy,alex,john,daniel,sarah,peter,lino}.
-                   #policy= {if_a_book_is_lost_the_borrower_must_pay_for_the_cost_of_the_book_plus_a_processing_fee}.
-                   #book={book1,book2}.
-                   #book_categories={drama, crime, fantasy, scientific}.
+                   #policy= {if_a_book_is_lost_the_borrower_must_pay_for_the_cost_of_the_book_plus_a_processing_fee, 
+                   books_that_are_within_the_return_period_can_be_returned_at_the_counter_or_dropped_off_in_the_designated_drop_off_box__If_a_book_is_overdue__it_must_be_returned_directly_to_the_library_counter__where_any_applicable_late_fees_will_be_assessed}.
+                   #information_type = {book, magazine, newspaper, journal, reference_book}.
+                   #book={book1,book2, macbeth, romeo_julit, outsider, the_six, load_of_the_rings, dune, beloved, kite_runner}.
+                   #book_categories={drama, crime, fantasy, scientific, fiction, story}.
+                   #book_type={general_books, textbooks}.
                    #day={monday, tuesday, wednesday, thursday, friday, saturday, sunday, weekdays, weekends}.
                    #hours={morning_8_to_6pm, morning_8_to_12pm}.
+                   #availability = {yes, no}.
+                   #number = 0..100.
+                   
                    predicates
-                   categories(#book_categories,#book).
+                   category(#book_categories,#book).
                    book_author(#people,#book).
                    opening_hours(#day,#hours).
                    lost_book_policy(#policy).
+                   return_policy(#policy).
+                   max_books(#number).
+                   ebook_available(#availability).
+                   information_type(#information_type).
+                   borrowing_period(#book_type, #number).
+                   
                    rules
                    opening_hours(weekdays, morning_8_to_6pm).
                    opening_hours(weekends, morning_8_to_12pm).
+                   max_books(3).
                    lost_book_policy(If_a_book_is_lost_the_borrower_must_pay_for_the_cost_of_the_book_plus_a_processing_fee). 
+                   return_policy(books_that_are_within_the_return_period_can_be_returned_at_the_counter_or_dropped_off_in_the_designated_drop_off_box__If_a_book_is_overdue__it_must_be_returned_directly_to_the_library_counter__where_any_applicable_late_fees_will_be_assessed).
+                   category(drama, macbeth).
+                   category(drama, romeo_julit).
+                   category(crime, outsider).
+                   category(crime, the_six).
+                   category(fantasy, load_of_the_rings).
+                   category(scientific, dune).
+                   category(fiction, beloved).
+                   category(story, kite_runner).
+                   ebook_available(yes).
+                   information_type(book).
+                   information_type(magazine). 
+                   information_type(newspaper). 
+                   information_type(journal). 
+                   information_type(reference_book).
+                   borrowing_period(general_books, 14).
+                   borrowing_period(textbooks, 7).
                    """}
 
     x = requests.post(url, headers=headers, data=payload)
